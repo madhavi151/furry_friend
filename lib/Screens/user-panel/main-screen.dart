7ae37517cffbf4a439a14043d5e90726bfca67c0
madhavi151 <madhavibalsara07@gmail.com>
2025-02-21 21:35:20 +0530

    added google sign in method

--- lib/Screens/user-panel/main-screen.dart
+++ lib/Screens/user-panel/main-screen.dart
@@ -1,6 +1,10 @@
 // ignore: file_names
 import 'package:flutter/material.dart';
+import 'package:furry_friend/Screens/auth-ui/welcome-screen.dart';
 import 'package:furry_friend/Utils/app-constant.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 
 class MainScreen extends StatelessWidget {
@@ -14,6 +18,19 @@ class MainScreen extends StatelessWidget {
         backgroundColor: AppConstant.appMainColor,
         title: Text(AppConstant.appMainName),
         centerTitle: true,
+        actions: [
+          GestureDetector(
+            onTap: (){
+              GoogleSignIn googleSignIn = GoogleSignIn();
+              googleSignIn.signOut();
+              Get.offAll(() => WelcomeScreen());
+            },
+            child: Padding
+            (padding: const EdgeInsets.all(8.0),
+            child: Icon(Icons.logout),
+            ),
+          )
+        ],
         ),
     );
   }
